3729deed934a32f7418ad8eb983b9f97e8761605
nvegas1230 <vboxuser@thehostname.testdomain.local>
2026-04-09 10:07:34 -0500

    Accidently made comments use 'echo' so i changed it to hashtags to make actual comments.

--- variables.sh
+++ variables.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'Single quotes are for literal strings, double quotes are for variable expansion'
+# Single quotes are for literal strings, double quotes are for variable expansion
 
-echo 'Because of overuse and unnecessary use of double quotes, people can break and abuse systems using command injection'
-echo 'Read command seems to return it in single quotes so it is realatively safe this way (from my understanding)'
+# Because of overuse and unnecessary use of double quotes, people can break and abuse systems using command injection
+# Read command seems to return it in single quotes so it is realatively safe this way (from my understanding)
