b35b07491546923910b54ece08b9fbe11337ab69
nvegas1230 <vboxuser@thehostname.testdomain.local>
2026-04-09 10:32:51 -0500

    Added more in-depth comments to my quotations.sh file to explain quotations.

--- quotations.sh
+++ quotations.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-# Single quotes are for literal strings, double quotes are for variable expansion
+# What are quotations?
+# --------------------
+# Quotes are a tool used to tell the computer that the lines are not literal commands, and are instead just characters
 
+# When do we use them?
+# --------------------
+# We use quotes when we want to either make something readable to humans, or when we want to put a line of code into another command
+# but not use it yet.
+
+# Types of quotations:
+# --------------------
+# There are only really two types of quotations:
+#	1) Single quotes
+#	2) Double quotes
+# Single quotes are used for LITERAL strings
+#	eg)	'This is a string'
+#		output -> This is a string
+# Double quotes are used for VARIABLE EXPANSION and strings
+#	eg)	"Your name is: $variable"
+#		output -> Your name is john 
+
+# Common mistakes:
+# --------------------
+# Misunderstanding when to use single and double quotes
+#	eg)	'Your name is: $variable'
+#		output -> Your name is $variable
+# Fix: make sure you use double quotes when you want to access a variable's value
+
+# Extra info:
+# --------------------
 # Because of overuse and unnecessary use of double quotes, people can break and abuse systems using command injection
 # Read command seems to return it in single quotes so it is realatively safe this way (from my understanding)
