a0113ea9ee76ba0a5b782c85ccd60d1d7874ba7e
nvegas1230 <vboxuser@thehostname.testdomain.local>
2026-04-09 10:09:59 -0500

    Accidently put the commash script (were in variables.sh originally).

--- variables.sh
+++ variables.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
 
-# Single quotes are for literal strings, double quotes are for variable expansion
-
-# Because of overuse and unnecessary use of double quotes, people can break and abuse systems using command injection
-# Read command seems to return it in single quotes so it is realatively safe this way (from my understanding)
