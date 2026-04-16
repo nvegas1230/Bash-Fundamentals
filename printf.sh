fa2cedb90481f9bc9885461aeca79e854d2c8196
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-16 10:27:01 -0500

    Added some comments to printf.sh

--- printf.sh
+++ printf.sh
@@ -2,14 +2,19 @@
 
 # What is printf()?
 # --------------------
-# 
+# printf is a built-in Bash command used to format and print output
+# It is basically echo but with more control over how text is displayed on the terminal
 #
 # When do we use it?
 # --------------------
-# 
-#
+# printf is used when you want specific formatting for your output
+# Examples include:
+#	Aligning text
+#	Controlling decimal places for numbers
+#	Printing variables in a specific format
 # Basic usage:
-# --------------------
+#	eg) printf "Hello"
+#		OUTPUT -> Hello
 # 
 #
 # Formatting options:
@@ -22,7 +27,11 @@
 #
 # Extra info:
 # --------------------
-# 
-#
+# printf does not automatically add a newline, unlike echo
+#	eg) echo "Hello"
+#		OUTPUT ->	Hello
+#				   >
+#	eg) printf "Hello"
+#		OUTPUT -> Hello>
 # COMMANDS TO DEMONSTRATE:
 # --------------------
