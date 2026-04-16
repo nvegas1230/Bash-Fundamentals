3a0316e96df068211b954948fabc43a3fb1a4039
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-16 10:20:40 -0500

    Added some comment structure to printf.sh

--- printf.sh
+++ printf.sh
@@ -1,1 +1,28 @@
 #!/bin/bash
+
+# What is printf()?
+# --------------------
+# 
+#
+# When do we use it?
+# --------------------
+# 
+#
+# Basic usage:
+# --------------------
+# 
+#
+# Formatting options:
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
