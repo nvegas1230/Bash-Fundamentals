bf764e77df649a49c79341e446ef8ad5e8662eef
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-21 10:39:56 -0500

    Added comment structure to brackkets.sh

--- brackets.sh
+++ brackets.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+# What are brackets?
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# How brackets work:
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
+#
