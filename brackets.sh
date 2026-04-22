9bd246fedbd60507da79014167e346e90e91bbe5
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-22 10:27:03 -0500

    Added some more comments/documentation to brackets.sh

--- brackets.sh
+++ brackets.sh
@@ -6,10 +6,6 @@
 # pattern matching, arithmetic, and command substitution
 # Different types of brackets have different meanings
 #
-# When do we use them?
-# --------------------
-#
-#
 # Types of brackets:
 # --------------------
 # ()    -> Subshell (runs commands in a separate environment)
@@ -20,17 +16,26 @@
 # $()   -> Command substitution (capture output)
 # ${}   -> Variable expansion/manipulation
 #
-# How brackets work:
+# When do we use them?
 # --------------------
-# 
+# We use brackets when we want to:
+#	Test conditions (if statements, comparisons)
+#	Grouping commands
+#	Performing arithmetic (math)
+#	Capturing command output (to assign to variables)
+#	Manipulating variables (mainly arrays)
 #
 # Common mistakes:
 # --------------------
-# 
+# 	Missing spaces: [ "$a"="$b" ] (wrong)
+# 	Using [ ] instead of [[ ]] for patterns
+# 	Forgetting semicolons inside {}
+# 	Confusing subshell () with grouping {}
+# 	Not using quotes around variables
 #
 # Extra info:
 # --------------------
-# 
+# Using [[]] for conditionals is usually safer and has more options
 #
 # COMMANDS TO DEMONSTRATE:
 # --------------------
