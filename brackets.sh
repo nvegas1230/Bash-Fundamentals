c4f75a71a38f16757d6fc53fb8300492bc1b9259
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-22 10:19:52 -0500

    Added some comments to quotations.sh to explain what they are and types of brackets

--- brackets.sh
+++ brackets.sh
@@ -2,11 +2,23 @@
 
 # What are brackets?
 # --------------------
-# 
+# Brackets in Bash are special symbols used for tests, grouping, expansion,
+# pattern matching, arithmetic, and command substitution
+# Different types of brackets have different meanings
 #
 # When do we use them?
 # --------------------
-# 
+#
+#
+# Types of brackets:
+# --------------------
+# ()    -> Subshell (runs commands in a separate environment)
+# {}    -> Group commands (same shell)
+# []    -> Basic test/condition (POSIX)
+# [[]]  -> Advanced test (Bash, supports patterns/regex)
+# (())  -> Arithmetic evaluation
+# $()   -> Command substitution (capture output)
+# ${}   -> Variable expansion/manipulation
 #
 # How brackets work:
 # --------------------
