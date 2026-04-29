99bd3c2c8ec8c3ba469e4ed85a2272422e985ac3
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-29 15:53:10 -0500

    Finished adding comment doccumentation to conditionals.sh and w moving on to actual commands demonstration

--- conditionals.sh
+++ conditionals.sh
@@ -37,14 +37,44 @@
 # Conditionals are used anytime you need to check a condition
 # This is useful for user input, pattern matching, looking for files, and much more
 # Conditionals are extremely important and will dictate the execution of a script
+# Conditionals must start with an 'if', have 'then' after the conditional, and end the block with 'fi'
+#	eg)	if [ condition ]; then
+#			:
+#		fi
+# You should also put a semicolon after the condition to state the line end
 #
 # Common mistakes:
 # --------------------
-# Using lots of elifs to check multiple conditions that are independant of eachother
+# Using elifs by themselves
+#	eg) elif [ condition ]; then
+#			:
+#		fi
+#	Fix: switch to an if statment (just remove the el from elif)
+# Placing multiple else statements
+#	eg) if [ condition ]; then
+#			:
+#		else
+#			:
+#		else
+#			:
+#		fi
+#	Fix: switch the extra else statments to elif or delete them
+# Forgetting to end with fi
+#	eg) if [ condition ]; then
+#			:
+#	Fix: add an 'fi' to indicate the end of the block
+# Leaving statements empty
+#	eg) if [ condition ]; then
+#			
+#		fi
+#	Fix: add a colon to the block, delete the conditional, or add code
 #
 # Extra info:
 # --------------------
-# 
+# Creating a function without code will error; use a colon to tell the script there is nothing to run
+#	eg) if [ condition ]; then
+#			: <- tells the computer that there is no code to run here
+#		fi
 #
 # COMMANDS TO DEMONSTRATE:
 # --------------------
