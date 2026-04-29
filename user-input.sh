853d466de80c2a0b0c3aefd645de0503bbe7652d
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-29 16:19:48 -0500

    Added basic description to user-input.sh

--- user-input.sh
+++ user-input.sh
@@ -2,7 +2,9 @@
 
 # What is user input?
 # --------------------
-# 
+# User input is any data provided by a user while a script is running.
+# In Bash, this is commonly done using the 'read' command, which allows
+# the script to pause and wait for the user to type something.
 #
 # Types of user input:
 # --------------------
