68d60cf0b65b2755256eef13d7c8613bafb10017
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-29 16:21:35 -0500

    Added some more comments to user-input.sh

--- user-input.sh
+++ user-input.sh
@@ -8,11 +8,30 @@
 #
 # Types of user input:
 # --------------------
-# 
+# 1) Basic input
+#    Stores input into a variable
+#    eg) read name
+#
+# 2) Prompted input (-p)
+#    Displays a message before accepting input
+#    eg) read -p "Enter your name: " name
+#
+# 3) Silent input (-s)
+#    Hides input (useful for passwords)
+#    eg) read -s password
+#
+# 4) Timed input (-t)
+#    Waits a certain number of seconds before timing out
+#    eg) read -t 5 input
+#
+# 5) Limited input (-n)
+#    Reads only a specific number of characters
+#    eg) read -n 1 choice
 #
 # When do we use it?
 # --------------------
-# 
+# User input is used whenever you want interaction in your script.
+# This includes:
 #
 # Common mistakes:
 # --------------------
