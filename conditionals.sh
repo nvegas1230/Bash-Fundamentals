74873c7601fff82dc58eb7f90217b4fe884288ea
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-29 10:33:51 -0500

    Renamed if-elif-else.sh to conditionals.sh and also added comment structure to file

---
+++ conditionals.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What are conditionals?
+# --------------------
+# 
+#
+# Types of conditionals:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 	
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
