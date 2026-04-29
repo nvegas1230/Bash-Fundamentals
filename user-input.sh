403d291ece6811ba0da59acd11f07276ed82469f
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-29 16:18:13 -0500

    Added comment strucutre and shebang to user-input.sh

---
+++ user-input.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What is user input?
+# --------------------
+# 
+#
+# Types of user input:
+# --------------------
+# 
+#
+# When do we use it?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
