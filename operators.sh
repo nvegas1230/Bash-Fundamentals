aee5b1861ba4b131dac4dd3cd46133c867cd3651
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-04-30 10:19:26 -0500

    A to operators.sh, moving onto command demonstration

--- operators.sh
+++ operators.sh
@@ -16,10 +16,12 @@
 #   ge (greater/equal)	eg) [ "$a" -ge "$b" ]
 #
 # Comparison operators (string, used for comparing text)
-#	=   (equal)			eg) [ "$a" = "$b" ]
-#   !=  (not equal)		eg) [ "$a" != "$b" ]
-#   -z  (empty string)	eg) [ -z "$a" ]
-#   -n  (not empty)		eg) [ -n "$a" ]
+#	==  (equal)					eg) [ "$a" == "$b" ]
+#   !=  (not equal)				eg) [ "$a" != "$b" ]
+#   -z  (empty string)			eg) [ -z "$a" ]
+#   -n  (not empty)				eg) [ -n "$a" ]
+#	>	(alphabetical order)	eg) [ "$a" > "$b" ]
+#	<	(alphabetical order)	eg) [ "$a" < "$b" ]
 #
 # Logical operators (Used to combine conditions)
 #	&&	(AND): 	both must be true
@@ -50,15 +52,25 @@
 #
 # When do we use them?
 # --------------------
-# 
+# We use operators whenever we have to check a conditional or do math
+# Essentially used everywhere in scripts that have any form of execution that relys on a condition
 #
 # Common mistakes:
 # --------------------
-# 
+# Some operators will be specifically for numbers, and others strings
+# It is easy to mistake the use case for each other
+#	eg) [ 10 < 2 ] will not work because the '<' sign is for comparing strings (in alphabetical order)
+# 	Fix: switch '<' to -lt to tell bash that they should be treated like numbers, not strings
+# Pattern matching operators will not work with single brackets
+#	eg) [ "$a" == *x ]
+#	Fix: switch to double brackets
 #
 # Extra info:
 # --------------------
-# 
+# Some operators can be combined (like equal (=) and greater than (>) to create equal to or greater than (>=))
+# The equal operator for strings (==) can be written with a single equal sign (=), but i dont like this
+# AND/OR operators (&&/||) cannot be used in single bracket conditionals, and you must use -a/-o instead
+# Single brackets will also not support pattern matching for checks
 #
 # COMMANDS TO DEMONSTRATE:
 # --------------------
