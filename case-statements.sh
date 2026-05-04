29aae40057439a0bba4fefffb8e15c7d3a549028
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-05-04 09:58:14 -0500

    Added comment structure to case-statements.sh

--- case-statements.sh
+++ case-statements.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+
+# What are case statements?
+# --------------------
+# 
+#
+# Types of case statements:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
