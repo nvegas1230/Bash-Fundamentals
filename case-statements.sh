888adf0b5582ff9926c0fa5e8f2c819b81735e22
nvegas1230 <nmullin@smail.pembinatrails.ca>
2026-05-04 10:05:12 -0500

    Added soome comments to case-statements.sh

--- case-statements.sh
+++ case-statements.sh
@@ -2,11 +2,23 @@
 
 # What are case statements?
 # --------------------
-# 
+# Case statements are conditionals that are an alternative to if-else-elif statements
+# Case statements are easier to expand on and are mainly for readability and efficiency
 #
 # Types of case statements:
 # --------------------
-# 
+# There is really only one type of case statement, but each line can be ended differently
+# 	case "$var" in <- var to use for matching goes between 'case' and 'in'
+#		1) <- If the var is equal to this number, it will use the command
+#		  echo 'var equals 1'
+#		  ;;
+#		2) <- Ditto
+#		  echo 'var equals 2'
+#		  ;;
+#		*) <- If the var is anything else, the star indicates 'else'
+#		  echo 'var equals something else'
+#		  ;;
+#	esac
 #
 # When do we use them?
 # --------------------
