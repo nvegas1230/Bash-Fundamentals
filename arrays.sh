d54b28ba5251cf5202aa085bf728d028a8e97281
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-04 10:41:16 -0500

    Added comment structure and shebang to arrays.sh

---
+++ arrays.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What are arrays?
+# --------------------
+# 
+#
+# Types of arrays:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
