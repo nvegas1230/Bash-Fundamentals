1ecde4dbe532f0b43890190e7429f1cee05854de
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-05 10:08:58 -0500

    Added associative array documentation to arrays.sh

--- arrays.sh
+++ arrays.sh
@@ -12,7 +12,7 @@
 # There are two main types of arrays in bash
 #	Indexed arrays:
 #		you would create an indexed array by assigning it to a variable
-#			eg) array_variable=(10, 2, 5, "hi", 7)
+#			eg) array_variable=(10 2 5 "hi" 7)
 #		or by initializing it and then assigning to it later
 #			eg) declare -a array_variable
 #		you can get elements from the array by their index or with @
@@ -20,8 +20,21 @@
 #				output -> 10
 #			eg) echo ${array_variable[@]}
 #				output -> 10 2 5 "hi" 7
-#	Associative arrays (dictionaries):
-#
+#	Associative arrays (a.k.a. dictionaries, Bash 4.0+):
+#		unlike indexed arrays, you can only make them by declaring them first
+#			eg) declare -A array_variable
+#		however, you can still assign to them right away at initialization
+#			eg) declare -A array_variable=([job]="chef" [pay]=50)
+#		you can add elements to the array by assigning or appending
+#			eg) array_variable[location]="pizza pizza"
+#				array_variable+=([location]="pizza pizza")
+#		you can get elements the same way as indexed arrays, except that the index
+#		is replaced by the element name
+#			eg) echo ${array_variable[job]}
+#				output -> chef
+#			eg) echo ${array_variable[@]}
+#				output -> chef 50
+#			
 # When do we use them?
 # --------------------
 # 
@@ -32,7 +45,7 @@
 #
 # Extra info:
 # --------------------
-# 
+# Array elements are seperated by space, not comma
 #
 # COMMANDS TO DEMONSTRATE:
 # --------------------
