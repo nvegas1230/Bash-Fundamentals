3d400ee7ff8f316a128a87e0d65e5c37b341348d
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-05 10:16:18 -0500

    Added even more ways to remove elements to arrays.sh

--- arrays.sh
+++ arrays.sh
@@ -15,11 +15,23 @@
 #			eg) array_variable=(10 2 5 "hi" 7)
 #		or by initializing it and then assigning to it later
 #			eg) declare -a array_variable
+#		you can add elements to the array by assigning or appending
+#			eg) array_variable[6]="hello"
+#				array_variable+=("hello")
+#		you can remove elements from an array by using 'unset'
+#			eg) unset array_variable (clears the array)
+#				unset array_variable[1] (clears element at index 1)
+#		there are some specific ways that could remove specific elements
+#			eg) my_array=( "${array_variable[@]/5/}" ) (removes  elements matching '5')
+#				my_array=("${my_array[@]}") (removes elements that are empty and rebuilds array)
 #		you can get elements from the array by their index or with @
 #			eg) echo ${array_variable[0]}
 #				output -> 10
 #			eg) echo ${array_variable[@]}
 #				output -> 10 2 5 "hi" 7
+#		you can get the number of elements by using # with all the elements
+#			eg) echo ${#array_variable[@]}
+#				output -> 5
 #	Associative arrays (a.k.a. dictionaries, Bash 4.0+):
 #		unlike indexed arrays, you can only make them by declaring them first
 #			eg) declare -A array_variable
@@ -34,6 +46,10 @@
 #				output -> chef
 #			eg) echo ${array_variable[@]}
 #				output -> chef 50
+#		you can get the number of elements in these arrays also, along with getting
+#		all of the names (or keys) of the elements
+#			eg) echo ${!array_varaible[@]}
+#				output -> job pay
 #			
 # When do we use them?
 # --------------------
