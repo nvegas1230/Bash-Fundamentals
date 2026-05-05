1b5fe74cd51a53c75d141568f76a7bf92760547e
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-05 09:56:01 -0500

    Added what arrays are in comments to arrays.sh

--- arrays.sh
+++ arrays.sh
@@ -2,7 +2,10 @@
 
 # What are arrays?
 # --------------------
-# 
+# Arrays are a type of data structure that stores more than value in one variable
+# In bash, arrays are only one-dimensional, which means you cannot nest arrays
+# Arrays can be intitialized with a specific variable type, or you can put multiple different
+# variable types into one array
 #
 # Types of arrays:
 # --------------------
