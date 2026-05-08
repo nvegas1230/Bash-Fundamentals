6dea6c6c6d5b08f60a691d7d7a53c6324df5732c
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-08 10:28:37 -0500

    Added shebang and comment structure to functions.sh

---
+++ functions.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What are functions?
+# --------------------
+# 
+#
+# Types of functions:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
