a4effac9264052cd91b2448bccbdbb579e3caca1
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-08 10:37:17 -0500

    Added some comments to functions.sh

--- functions.sh
+++ functions.sh
@@ -2,10 +2,24 @@
 
 # What are functions?
 # --------------------
-# 
+# Functions are blocks of code that you can define and then run later
+# Functions can also take in some arguments that will change how they function
 #
 # Types of functions:
 # --------------------
+# Two main types of functions
+#	Builtin (predefined) functions:
+#		Functions that bash has built in to the language
+#		eg) echo, printf, cd, etc.
+#	Custom (user-defined) functions
+#		Functions that you make for yourself/scripts
+#		eg)	example_function() {
+#				:
+#			}
+#			OR
+#			function example_function {
+#				:
+#			}
 # 
 #
 # When do we use them?
