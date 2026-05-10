e5b395f047fb8bc980e9d40779e9744aa1203125
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 14:18:48 -0500

    Added shebang and comment structure to for-loops.sh

---
+++ for-loops.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What are for-loops?
+# --------------------
+# 
+#
+# Types of for-loops:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+#
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
