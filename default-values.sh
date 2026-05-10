d7686e874f2e96fce9ac3d0e274edb628bb82b99
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 15:16:04 -0500

    Added shebang and comment structure to default-values.sh

---
+++ default-values.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What are default values?
+# --------------------
+# 
+#
+# Types of default values:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
