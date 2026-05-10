1825439046954daffc11622ae228600c55f51a54
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 14:47:40 -0500

    Added shebang and comment structure to while-loops.sh

---
+++ while-loops.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What are while-loops?
+# --------------------
+# 
+#
+# Types of while-loops:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
