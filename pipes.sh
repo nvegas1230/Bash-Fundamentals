3c8b8712fc452739a30af1a3cd6040040b892b2e
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 15:41:31 -0500

    Added some basic comments to pipes.sh

---
+++ pipes.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# What are pipes?
+# --------------------
+# Pipes are used to take the output from one command and input it into another command
+#
+# Types of pipes:
+# --------------------
+# Pipes are pretty simple, and the complexity of pipes are actually just the combinations you can make
+# A basic example is grep, where it will take the input and sort it with grep
+#	eg) ls | grep example_directory
+#		(instead of displaying all parts of the directory, it will filter out anything except for 'example_directory')
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
