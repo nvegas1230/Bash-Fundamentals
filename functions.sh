7a1c7d06926eda99ce2fe7e7b4fadf327210882f
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 13:43:39 -0500

    Added some comments to functions.sh

--- functions.sh
+++ functions.sh
@@ -31,10 +31,15 @@
 #		want your functions to all have some sort of common output handling,
 #		you should use a wrapper function
 # 	Recursive functions:
+#		These functions are for recursively searching through something
+#		or anything of the sort
+#		They will call themselves in the function
 #	Exported functions:
 #		When you use export -f <function>, you can ake a function available
 #		to subshells and child processes
 #	Nested functions
+#		Nested functions are simply functions that only need to be used within
+#		a specific block of code (like inside of a big if statement)
 #
 # When do we use them?
 # --------------------
