0d47bf89303013547c04d9b12ae33c9836fe844e
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 16:06:48 -0500

    Added shebang and comment structure to redirects.sh

---
+++ redirects.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What are redirects?
+# --------------------
+# 
+#
+# Types of redirects:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
