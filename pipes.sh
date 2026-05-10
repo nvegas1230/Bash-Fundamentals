0ca47dc514c3358892908dedf9c4c868e6854514
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 15:45:20 -0500

    Added more comments to pipes.sh

--- pipes.sh
+++ pipes.sh
@@ -13,15 +13,30 @@
 #
 # When do we use them?
 # --------------------
-# 
+# We use pipes whenever we want one command to use the output of another command
+# They are useful for filtering, searching, sorting, counting, formatting, etc.
 #
 # Common mistakes:
 # --------------------
-# 
+# Forgetting spaces around the pipe symbol
+#	eg) echo "Hello"|wc
+#	Fix: Add spaces around the pipe
+#		echo "Hello" | wc
+# Using pipes when output redirection is needed
+#	eg) echo "Hello" | file.txt
+#	Fix: Use output redirection instead
+#		echo "Hello" > file.txt
+# Using commands that do not read standard input
+#	eg) cd test | wc
+#	Fix: Use commands that accept piped input
+#		echo "test" | wc
 #
 # Extra info:
 # --------------------
-# 
+# The pipe operator is '|'
+# Pipes send standard output (stdout) into standard input (stdin)
+# Pipes can chain many commands together
+# Commands connected with pipes run at the same time
 #
 # COMMANDS TO DEMONSTRATE:
 # --------------------
