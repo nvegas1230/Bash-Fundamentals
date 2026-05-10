07e44c800d77f443154a30ed2198108e36c63fac
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 14:35:04 -0500

    Fixed some comments where I put an extra # and i thought it looked weird

--- for-loops.sh
+++ for-loops.sh
@@ -8,28 +8,27 @@
 # --------------------
 # List-based for-loops:
 #	Loops through a list of values
-#	eg) # fruits="apple banana orange"
+#	eg) fruits="apple banana orange"
 #		for item in $fruits; do
 #			echo "$item"
 #		done
 #
 # Range-based for-loops:
 #	Loops through a range of numbers
-#	eg) # No variables needed
-#		for num in {1..5}; do
+#	eg) for num in {1..5}; do
 #			echo "$num"
 #		done
 #
 # C-style for-loops:
 #	Loops similarly to languages like C/Java
-#	eg) # start=1, end=5
+#	eg) (start=1, end=5)
 #		for (( i=1; i<=5; i++ )); do
 #			echo "$i"
 #		done
 #
 # File-based loops:
 #	Loops through files or directories using pattern matching
-#	eg) # Looks for all .txt files in the current directory
+#	eg) (Looks for all .txt files in the current directory)
 #		for file in *.txt; do
 #			echo "$file"
 #		done
