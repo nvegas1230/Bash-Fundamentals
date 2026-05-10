dd00347a65b1cddc75703ee27597387b809e9e2c
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 14:48:14 -0500

    Added basic definition to while-loops.sh

--- while-loops.sh
+++ while-loops.sh
@@ -2,7 +2,8 @@
 
 # What are while-loops?
 # --------------------
-# 
+# While-loops are blocks of code that will continue running as long as a condition is true
+# They are commonly used when you do not know exactly how many times a loop should run
 #
 # Types of while-loops:
 # --------------------
