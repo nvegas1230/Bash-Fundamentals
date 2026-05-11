67e630d2c84d7caf229ba7b87f4ea7a4d4e15bc7
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 21:59:17 -0500

    Added some comments to two-dimensional-arrays.sh

--- two-dimensional-arrays.sh
+++ two-dimensional-arrays.sh
@@ -2,7 +2,9 @@
 
 # What are two dimensional arrays?
 # --------------------
-# 
+# Two dimensional arrays store data in rows and columns
+# Bash does not directly support actual 2D arrays, but we can fake them
+# with certain methods (like associative arrays)
 #
 # Types of two dimensional arrays:
 # --------------------
