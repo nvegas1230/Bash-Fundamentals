81e7448aa4ea09ef0b50444d01bd1643e5cece31
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 22:06:27 -0500

    Added some types/ideas to two-dimensional-arrays.sh

--- two-dimensional-arrays.sh
+++ two-dimensional-arrays.sh
@@ -3,12 +3,27 @@
 # What are two dimensional arrays?
 # --------------------
 # Two dimensional arrays store data in rows and columns
+# Two dimensional arrays also may have one array in another array
 # Bash does not directly support actual 2D arrays, but we can fake them
 # with certain methods (like associative arrays)
 #
 # Types of two dimensional arrays:
 # --------------------
-# 
+# Associative arrays:
+#	Uses keys that look like 2 points but are actually just one string
+#	eg) declare -A table
+#		table[1,2]="B"
+#
+# Turning arrays into strings:
+#	Use a string with values seperated by spaces that can then be turned into
+#	its own array if you iterate through the values
+#	eg) declare -A table
+#		string="apple orange banana grape"
+#		table[fruits] = "$string"
+#		IFS=" " read -ra fruits <<< "${table[fruits]}"
+#		for fruit in "${fruits[@]}"; do
+# 			echo "Processing: $fruit"
+#		done
 #
 # When do we use them?
 # --------------------
