86f3faaf3052f0bcd5d33d3fed144b9cf6a7a89c
nvegas1230 <197675519+nvegas1230@users.noreply.github.com>
2026-05-10 21:56:53 -0500

    Added shebang and comment structure to two-dimensional-arrays.sh

---
+++ two-dimensional-arrays.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# What are two dimensional arrays?
+# --------------------
+# 
+#
+# Types of two dimensional arrays:
+# --------------------
+# 
+#
+# When do we use them?
+# --------------------
+# 
+#
+# Common mistakes:
+# --------------------
+# 
+#
+# Extra info:
+# --------------------
+# 
+#
+# COMMANDS TO DEMONSTRATE:
+# --------------------
